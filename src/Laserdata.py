b2845f579d4422548b3a5649a82c691e8badcd29
TUoS Student <student@TENB885849C54C2.shefuniad.shef.ac.uk>
2023-05-02 18:20:04 +0100

    Correcting use of mean() instead of min()

--- src/Laserdata.py
+++ src/Laserdata.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-from statistics import mean
 import rospy
 from sensor_msgs.msg import LaserScan
+from statistics import mean
 # from laser_filters.srv import LaserScanMLSFilter
 
 class LaserData(object):
     def laserscan_cb(self, msg):
-        self.front_dis = round(mean(min(msg.ranges[0:15]), min(msg.ranges[345:359])), 2)
+        self.front_dis = round(min(mean(msg.ranges[0:15]), mean(msg.ranges[345:359])), 2)
         self.left_dis = round(mean(msg.ranges[75:105]), 2)
         self.right_dis = round(mean(msg.ranges[255:285]), 2)
         self.back_dis = round(mean(msg.ranges[145:215]), 2)
         self.right_lower = round(mean(msg.ranges[255:270]), 2) # PID second stage switch
         self.right_upper = round(mean(msg.ranges[282:285]), 2) # PID second stage switch
-        self.front_obs = round(mean(min(msg.ranges[0:30]), min(msg.ranges[330:359])), 2)
+        self.front_obs = round(min(mean(msg.ranges[0:30]), mean(msg.ranges[330:359])), 2)
     def __init__(self):
         self.front_dis = 0
         self.left_dis = 0
